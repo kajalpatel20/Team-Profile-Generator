4c1e28b62df06d6c5390a66f77ac036d01132723
Kajal Patel <kajalpatel20@gmail.com>
2021-03-03 22:07:45 -0500

    add html page and css

--- test/Manager.test.js
+++ test/Manager.test.js
@@ -3,23 +3,23 @@ const Manager = require("../lib/Manager");
 // set office number using constructor
 
 test("set office number using constructor argument", () => {
-    const officenumber = 1;
-    const m = new Manager(officenumber);
-    expect(m.officenumber).toBe(officenumber)
+    const name = 11;
+    const m = new Manager(name);
+    expect(m.getOfficeNumber).toBe(name)
 });
 
 //getrole method should return "Manager"
 
 test("get role using getRole()", () => {
     const role = "Manager";   
-     const m = new Manager("testName", 123, "test@test.com",1);
+     const m = new Manager("testName", 123, "test@test.com",11);
     expect(m.getRole()).toBe(role)
 })
 
 //get office number using getOfficeNumber method
 test("get office number using getOfficenumber() method", () => {
-    const officenumber = 1;
-    const m = new Manager("testName", 123, "test@test.com", 1);
-    expect(m.getOfficenumber()).toBe(officenumber);
+    const name = 11;
+    const m = new Manager("testName", 123, "test@test.com",11);
+    expect(m.getOfficeNumber()).toBe(name);
   });
   
